52986e333e56efbab87f3727a37595012a36f76e
Chan Vic <1093332977@qq.com>
2017-07-16 13:28:41 +0800

    Test the ListReverse

--- 源.cpp
+++ 源.cpp
@@ -1,6 +1,16 @@
 #include"List.h"
+#include"Puzzle.h"
+
+#define N 7
 
 int main() {
-	List L;
+	List L = MakeEmpty(NULL);
+	for (int i = 0; i < N; i++) {
+		Insert(N - i, L, L);
+	}
+	PrintList(L);
+	ListReverse3(&L);
+	printf("\n");
+	PrintList(L);
 	return 0;
 }
